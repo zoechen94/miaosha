a46cf92c98b3fd106b41c8814f0064c0cb2c0a5f
chenyalan <chenyalan@andlinks.com>
2018-12-12 21:19:40 +0800

    忽略文件

--- src/main/java/com/demo/miaoshao/App.java
+++ src/main/java/com/demo/miaoshao/App.java
@@ -1,9 +1,15 @@
 package com.demo.miaoshao;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 /**
  * @author: 喜🐑
  * @create: 2018-12-12 21:13
  */
-
+@EnableAutoConfiguration
 public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class);
+    }
 }
